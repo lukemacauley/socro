66a92df8d3f3999ca1617a67937ce93cf7ecd873
Luke Macauley <lukemacauley@mac.com>
2025-07-21 20:12:44 +0200

    fix oauth

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,14 +1,19 @@
-import { SignIn } from "@clerk/react-router";
+import { SignIn, SignUp } from "@clerk/react-router";
 import type { Route } from "./+types/home";
 
 export default function Home({}: Route.ComponentProps) {
   return (
-    <div className="flex items-center justify-center h-screen w-full">
+    <div className="flex items-center justify-center h-screen gap-8 w-full">
       <SignIn
         oauthFlow="popup"
         afterSignOutUrl="/"
         fallbackRedirectUrl="/threads"
-        withSignUp
+        withSignUp={true}
+      />
+      <SignUp
+        oauthFlow="popup"
+        afterSignOutUrl="/"
+        fallbackRedirectUrl="/threads"
       />
     </div>
   );
